81f10db3201c7939a670f1cde10960e9e0169302
samuelchen1213 <samuelchen23@icloud.com>
2019-05-10 00:48:04 -0400

    😆Initlialized MongoDB, Express, and Routes

--- Server.js
+++ Server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
 const app = express();
 
 // DB Config
@@ -12,7 +16,12 @@ mongoose
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello!git "));
+app.get("/", (req, res) => res.send("Hello!"));
+
+// Use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
