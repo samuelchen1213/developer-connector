6b7b814dea1dbeb184901aec95091128b606e2a3
samuelchen1213 <samuelchen23@icloud.com>
2019-05-14 21:04:06 -0400

    🐜 Currently fixing bug (register and login)

--- Server.js
+++ Server.js
@@ -1,34 +1,34 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const passport = require("passport");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
-const users = require("./routes/api/users");
-const profile = require("./routes/api/profile");
-const posts = require("./routes/api/posts");
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
-// Body parser middlware (Now you can access body.etc)
+// Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // DB Config
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
-    .connect(db, { useNewUrlParser: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 // Passport middleware
 app.use(passport.initialize());
 
-// Passport config
+// Passport Config
 require('./config/passport')(passport);
 
-// Use routes
+// Use Routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
