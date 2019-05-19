1c5a6ec76f2c2bbfa7ef1c47c540c69f8031641e
samuelchen1213 <samuelchen23@icloud.com>
2019-05-18 21:02:00 -0400

    💾Completed Profile Schema

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,8 +7,8 @@ const keys = require('../../config/keys');
 const passport = require('passport');
 
 // Load Input Validation
-const validateRegisterInput = require('../../config/validation/register');
-const validateLoginInput = require('../../config/validation/login');
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // Load User model
 const User = require('../../models/User');
@@ -22,12 +22,12 @@ router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
 // @desc    Register user
 // @access  Public
 router.post('/register', (req, res) => {
-//   const { errors, isValid } = validateRegisterInput(req.body);
+  const { errors, isValid } = validateRegisterInput(req.body);
 
-//   // Check Validation
-//   if (!isValid) {
-//     return res.status(400).json(errors);
-//   }
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
 
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
@@ -65,12 +65,12 @@ router.post('/register', (req, res) => {
 // @desc    Login User / Returning JWT Token
 // @access  Public
 router.post('/login', (req, res) => {
-//   const { errors, isValid } = validateLoginInput(req.body);
+  const { errors, isValid } = validateLoginInput(req.body);
 
-//   // Check Validation
-//   if (!isValid) {
-//     return res.status(400).json(errors);
-//   }
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
 
   const email = req.body.email;
   const password = req.body.password;
