4a3328fae9113f53cccaa03fb7f2e1ca51391e09
samuelchen1213 <samuelchen23@icloud.com>
2019-05-18 21:56:39 -0400

    💾Completed GET Profile

--- models/Profile.js
+++ models/Profile.js
@@ -2,128 +2,119 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Create Schema
-const ProfileSchema = new Schema ({
-    user: {
-        // Associate user by ID
-        type: Schema.Types.ObjectId,
-        ref: 'users'
-    },
-    
-    // SEO friendly handle
-    handle: {
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 40
+  },
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  githubusername: {
+    type: String
+  },
+  experience: [
+    {
+      title: {
         type: String,
-        required: true,
-        max: 40
-    },
-
-    company: {
-        type: String
-    },
-
-    website: {
-        type: String
-    },
-
-    location: {
-        type: String
-    },
-
-    status: {
-        required: String,
         required: true
-    },
-
-    skills: {
-        type: [String],
+      },
+      company: {
+        type: String,
         required: true
-    },
-
-    bio: {
+      },
+      location: {
         type: String
-    },
-
-    github: {
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
         type: String
-    },
-
-    experience: [{
-        title: {
-            type: String, 
-            required: true
-        },
-        company: {
-            type: String, 
-            required: true
-        },
-        location: {
-            type: String, 
-        },
-        from: {
-            type: Date, 
-            required: true
-        },
-        to: {
-            type: Date, 
-        },
-        current: {
-            type: Boolean,
-            default: false 
-        },
-        description: {
-            type: String
-        }
-    }],
-
-    education: [{
-        school: {
-            type: String, 
-            required: true
-        },
-        degree: {
-            type: String, 
-            required: true
-        },
-        fieldofstudy: {
-            type: String, 
-            required: true
-        },
-        from: {
-            type: Date, 
-            required: true
-        },
-        to: {
-            type: Date, 
-        },
-        current: {
-            type: Boolean,
-            default: false 
-        },
-        description: {
-            type: String
-        }
-    }],
-
-    social: {
-        youtube: {
-            type: String,
-        },
-        twitter: {
-            type: String,
-        },
-        facebook: {
-            type: String,
-        },
-        linkedin: {
-            type: String,
-        },
-        instagram: {
-            type: String,
-        },
-        date: {
-            type: Date,
-            default: Date.now
-        }
+      }
     }
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofstudy: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-module.exports =  Profile = mongoose.model('profile', ProfileSchema);
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
