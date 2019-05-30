b60b3c30d1b5d2e6d925e4451093aca2354447a0
samuelchen1213 <samuelchen23@icloud.com>
2019-05-30 17:25:58 -0400

    Completed Login and Auth Reducer (Redux)

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
+
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
 
 // Register
 export const registerUser = (data, history) => (dispatch) => {
@@ -11,4 +14,40 @@ export const registerUser = (data, history) => (dispatch) => {
                 payload: err.response.data
             })
         );
+}
+
+// Login ( Get User Token )
+export const loginUser = data => (dispatch) => {
+    axios.post('/api/users/login', data)
+        .then(res => {
+            // Save to local storage
+            const { token } = res.data;
+
+            // Set token to local storage
+            localStorage.setItem('jwtToken', token);
+
+            // Set token to Auth Header
+            setAuthToken(token);
+
+            // Decode Token
+            const decoded = jwt_decode(token);
+
+            // Set current user
+            dispatch(setCurrentUser(decoded));
+
+        })
+        .catch (err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data  
+            })
+        )
+}
+
+// Set logged in user
+export const setCurrentUser = decoded => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    }
 }
