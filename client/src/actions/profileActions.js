438603c5ef9a86b2508f8a5f9a1c088515b20620
samuelchen1213 <samuelchen23@icloud.com>
2019-06-10 19:43:14 -0400

    💾Began Profiles Display

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -5,7 +5,8 @@ import {
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
   GET_ERRORS,
-  SET_CURRENT_USER
+  SET_CURRENT_USER,
+  GET_PROFILES
 } from './types';
 
 // Get current profile
@@ -62,6 +63,25 @@ export const addEducation = (eduData, history) => dispatch => {
               payload: err.response.data
           }
       ));
+};
+
+// Get all profiles
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get('/api/profile/all')
+    .then(res =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: null
+      })
+    );
 };
 
 // Delete experience
