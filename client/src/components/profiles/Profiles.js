9bff67e444a95b81b9a1a59c46da635e3e8c97c3
samuelchen1213 <samuelchen23@icloud.com>
2019-06-14 22:48:05 -0400

    💾Display Profiles

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import { getProfiles } from '../../actions/profileActions';
 
 import Spinner from '../common/Spinner';
+import ProfileItem from './ProfileItem';
 
 class Profiles extends Component {
   componentDidMount() {
@@ -18,7 +19,9 @@ class Profiles extends Component {
       profileItems = <Spinner />;
     } else {
       if (profiles.length > 0) {
-        profileItems = <h1>PROFILE</h1>
+        profileItems = profiles.map(profile => (
+          <ProfileItem key={profile._id} profile={profile}/>
+        ))
       } else {
         profileItems = <h4>No profiles found...</h4>;
       }
